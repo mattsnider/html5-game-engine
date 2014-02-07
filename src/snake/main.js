cc7ed823279483881f208242f1c843313cd5892e
mattsnider <mattesnider@gmail.com>
2014-02-07 12:12:43 -0800

    preventing default key behavior in snake game

--- src/snake/main.js
+++ src/snake/main.js
@@ -7,9 +7,11 @@
   // todo: move this into a game controller system
   $(document).keydown(function(e) {
     game.setKey(e.keyCode);
+    e.preventDefault();
   });
 
   $(document).keyup(function(e) {
     game.setKey(0);
+    e.preventDefault();
   });
 }());
